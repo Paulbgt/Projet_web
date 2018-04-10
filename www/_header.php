2ae01c709ceb7a012366cfa2c5488d948a721ac2
Aurélien Klein <aurelien.klein@viacesi.fr>
2018-04-11 01:25:49 +0200

    Responsive page d'acceuil

--- www/_header.php
+++ www/_header.php
@@ -1,5 +1,4 @@
-<div class="AKL-ctn--c1 header">
-    
+<div class="AKL-ctn--c1-s0 header">
     <div class="nav-btn">
         <svg id="nav-btn-svg" width="40" height="30">
           <rect x="0" y="0" width="60" height="4"/>
@@ -29,4 +28,35 @@
         <img class="header-logout" src="img/logout2.svg" alt="Déconnexion">
         <img class="header-logout" src="img/logout1.svg" alt="Déconnexion">
     </a>
+</div>
+
+
+<div class="AKL-ctn--c0-s1 headerPhone">
+    <span class="headerPhone-title">Site du BDE</span>
+    <span class="headerPhone-spanExia">Exia.Cesi Arras</span>
+    <div class="navPhone-btn">
+        <svg id="navPhone-btn-svg" width="40" height="30">
+          <rect x="0" y="0" width="60" height="4"/>
+          <rect x="0" y="13" width="60" height="4"/>
+          <rect x="0" y="26" width="60" height="4"/>
+        </svg>
+    </div>
+    <div class="navPhone-blc">
+        <ul class="nav-blc-ul">
+            <li class="nav-blc-ul-li"><a>Boite à idée</a></li>
+            <li class="nav-blc-ul-li"><a>Evénements du mois</a></li>
+            <li class="nav-blc-ul-li"><a>Evénements passés</a></li>
+            <li class="nav-blc-ul-li"><a>Boutique</a></li>
+        </ul>
+        <hr>
+        <span class="headerPhone-log-name">Aurélien KLEIN</span>
+        <span class="headerPhone-log-span">Statut : Etudiant</span>
+        
+        <a>
+            <img class="headerPhone-logout" src="img/logout2.svg" alt="Déconnexion">
+            <img class="headerPhone-logout" src="img/logout1.svg" alt="Déconnexion">
+        </a>
+    </div>
+    
+    
 </div>
--- www/js/common.js
+++ www/js/common.js
@@ -1,5 +1,7 @@
 var navButton = document.getElementById('nav-btn-svg');
+var navButtonPhone = document.querySelector('.navPhone-btn');
 var navBloc = document.querySelector('.nav-blc');
+var navBlocPhone = document.querySelector('.navPhone-blc');
 var showMenu = false;
 
 
@@ -23,6 +25,36 @@ navButton.addEventListener('click', function() {
         setTimeout(function() {
             navBloc.children[0].style.display = "none";
         }, 500);
+        showMenu = false;
+    }
+});
+
+navButtonPhone.addEventListener('click', function() {
+    if (!showMenu) {
+        this.children[0].children[0].style.transform = "rotate(33deg)";
+        this.children[0].children[1].style.transform = "scale(0)";
+        this.children[0].children[2].style.transform = "rotate(-33deg)";
+        this.style.backgroundColor = "var(--exia-red2)";
+//        navBloc.style.transform = "scale(1)";
+        navBlocPhone.style.height = "auto";
+        var h = navBlocPhone.offsetHeight;
+        navBlocPhone.style.height = "0";
+        setTimeout(function(){
+            navBlocPhone.style.height = h + "px";
+        }, 1);
+        
+//        navBlocPhone.children[0].style.display = "block";
+        showMenu = true;
+    } else {
+        this.children[0].children[0].style.transform = "rotate(0)";
+        this.children[0].children[1].style.transform = "scale(1)";
+        this.children[0].children[2].style.transform = "rotate(0)";
+        this.style.backgroundColor = "transparent";
+//        navBloc.style.transform = "scale(0)";
+        navBlocPhone.style.height = "0";
+//        setTimeout(function() {
+//            navBlocPhone.children[0].style.display = "none";
+//        }, 500);
         showMenu = false;
     }
 });
