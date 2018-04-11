55fe17efe4112a2a3622689b019aaa079eb2d2d2
anthonydescamps <anthony.descamps@viacesi.fr>
2018-04-11 15:59:39 +0200

    suggestion pour les événements (manque la session utilisateur)

---
+++ www/php/suggest_event.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Suggestion de statut</title>
+	<meta charset="utf-8">
+</head>
+<body>
+<h1>Suggestion d'événement</h1>
+<form action="suggest_event_add.php" method="POST">
+	<input type="text" name="title" id="title">
+
+	<br><br>
+
+	<input type="text" name="description" id="description">
+
+
+	<br><br>
+
+	<p>Date de l'événement</p>
+	<input type="date" name="event_date" id="event_date">
+	
+	<br><br>
+
+	<input type="submit" name="validation date_event">
+
+
+
+
+</form>
+
+
+
+</body>
+</html>
---
+++ www/php/suggest_event_add.php
@@ -0,0 +1,40 @@
+<?php  
+try{
+//conexion à la base de données
+$bdd = new PDO('mysql:host=localhost;dbname=webproject;charset=utf8', 'root', '');
+
+} catch(PDOException $e){
+
+	die($e->getMessage());
+
+}
+//on définit les variables avec ce que l'utilisateurs a rempli dans le formulaire
+$title = $_POST['title'];
+$description = $_POST['description'];
+$event_date = $_POST['event_date'];
+
+//on vérifie que le champs ne sont pas vide avant de remplir la base de données
+if (!empty($title) && !empty($description) && !empty($event_date)) {
+
+//si tous les champs sont remplis alors on inscrit les données dans la BDD 
+$add = $bdd->prepare("INSERT INTO happening(title, description, event_date) VALUES(:title, :description, :event_date)");
+$add->execute([
+    'title' => $title,
+    'description' => $description,
+    'event_date' => $event_date
+]);
+
+//permet de savoir si il y a des erreurs dans notre requête
+$add->errorInfo();
+
+//message si l'insertion s'est bien passée
+echo "insertion dans la base de données";
+
+}else{
+
+//message si l'insertion dans la base de données ne s'effectue pas
+echo "l'événement n'a pas été ajouté dans la base de données.";
+
+
+}
+?>
