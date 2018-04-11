86b87ea038f7e148a7a935770d05c6c184b6c891
Aurélien Klein <aurelien.klein@viacesi.fr>
2018-04-11 09:39:04 +0200

    fusion validation et front

--- www/js/indexValidation.js
+++ www/js/indexValidation.js
@@ -1,20 +1,22 @@
 var validationRegister = document.getElementById('submitRegister');
 var validationConnection = document.getElementById('submitConnection');
 var inputRegister = document.querySelectorAll('[class*=form-register-input]');
+var formError = document.querySelectorAll('.form-error');
 var lastName = document.getElementById('lastName');
 var firstName = document.getElementById('firstName');
 var passwordRegister = document.getElementById('passwordRegister');
 var mailRegister = document.getElementById('mailRegister');
 
 validationRegister.addEventListener('click', function(e) {
+    for (var i = 0; i<formError.length; i++) {
+        formError[i].style.opacity = 0;
+    }
 	for(var i = 0 ; i<inputRegister.length ; i++ ){
-		console.log(i);
 		switch(inputRegister[i].type) {
 		case 'text' : checkText(inputRegister[i], e); break;
 		case 'password' : checkPassword(inputRegister[i], e); break;
 		case 'email' : checkMail(inputRegister[i], e); break;
 		}
-
 	}
 });
 
@@ -45,7 +47,6 @@ function checkMail(element, e){
 	}
 }
 
-function displayError(element, e){
-	element.classList.add(element.classList[1].replace('input', 'input--error'));
-	element.classList.remove(element.classList[1]);
+function displayError(element){
+    document.querySelector('[for=' + element.id + ']').style.opacity = 1;
 }
