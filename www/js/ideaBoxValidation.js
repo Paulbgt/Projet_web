57a599fd9d36438de9ef4af6b501e6e5c15e4fd1
florianfritsch <florian.fritsch@viacesi.fr>
2018-04-13 11:43:56 +0200

    ideaBoxValidation.js

--- www/js/ideaBoxValidation.js
+++ www/js/ideaBoxValidation.js
@@ -1,23 +1,26 @@
 var validationIdea = document.getElementById('submitIdea');
-var inputTitle = document.querySelectorAll('[class*=suggestion-infos-title]');
-var inputDescription = document.querySelectorAll('[class*=suggestion-infos-description]');
+var inputTitle = document.querySelector('.suggestion-infos-title');
+var inputDescription = document.querySelector('.suggestion-infos-description');
 var formError = document.querySelectorAll('.form-error');
 
+//Verify if the field is correct when we click on the register button and check the type of field
 validationIdea.addEventListener('click', function(e) {
-    formError[i].style.opacity = 0;
-    checkText(inputTitle[i], e);
-    checkText(inputDescription[i], e);
+    for (var i = 0; i<formError.length; i++) {
+        formError[i].style.opacity = 0;
+    }
+		checkText(inputTitle, e);
+		checkText(inputDescription, e);
 });
 
+//Check the text field and prevent the form from being sent
 function checkText(element, e){
-	var regex = /^[a-zA-Z][a-z]+([-'\s][a-zA-Z])?/;
-
-	if(regex.test(element.value) == false){
+	if(element.value == ""){
 		e.preventDefault();
 		displayError(element);
 	}
 }
 
+//Display an error in the incorrect field
 function displayError(element){
     document.querySelector('[for=' + element.id + ']').style.opacity = 1;
 }
