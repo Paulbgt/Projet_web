a6b1b5299f524b79ddb0d17f3655eef124640cdd
Paulbgt <paul.boogaerts@viacesi.fr>
2018-04-13 11:08:13 +0200

    dernière update avec catégorie et commande modifié

--- code/bdd/bdd_script.sql
+++ code/bdd/bdd_script.sql
@@ -13,23 +13,7 @@ CREATE TABLE account(
         first_name Char (255) NOT NULL ,
         mail       Varchar (255) NOT NULL ,
         pwd        Varchar (255) NOT NULL ,
-        statute    Int (10) DEFAULT 0,
-        PRIMARY KEY (id )
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: selling
-#------------------------------------------------------------
-
-CREATE TABLE selling(
-        id         int (11) Auto_increment  NOT NULL ,
-        indent     Char (25) NOT NULL ,
-        dated_sale TimeStamp NOT NULL ,
-        quantity   Int NOT NULL ,
-        statute    Char (25) NOT NULL ,
-        id_produce Int NOT NULL ,
-        id_account Int NOT NULL ,
+        statute    Int (11) DEFAULT 0 ,
         PRIMARY KEY (id )
 )ENGINE=InnoDB;
 
@@ -42,8 +26,8 @@ CREATE TABLE happening(
         id            int (11) Auto_increment  NOT NULL ,
         title         Varchar (255) NOT NULL ,
         description   Text NOT NULL ,
-        finished      Bool ,
-        validate      Bool NOT NULL ,
+        finished      Boolean ,
+        validate      Boolean NOT NULL ,
         event_date    Date ,
         publish_dated TimeStamp NOT NULL ,
         id_account    Int NOT NULL ,
@@ -69,6 +53,7 @@ CREATE TABLE commentary(
 CREATE TABLE produce_picture(
         id         int (11) Auto_increment  NOT NULL ,
         url        Varchar (255) NOT NULL ,
+        ref        Boolean ,
         id_produce Int NOT NULL ,
         PRIMARY KEY (id )
 )ENGINE=InnoDB;
@@ -84,6 +69,7 @@ CREATE TABLE produce(
         description Varchar (255) NOT NULL ,
         category    Varchar (255) NOT NULL ,
         price       Int NOT NULL ,
+        id_category Int NOT NULL ,
         PRIMARY KEY (id )
 )ENGINE=InnoDB;
 
@@ -95,8 +81,32 @@ CREATE TABLE produce(
 CREATE TABLE event_picture(
         id           int (11) Auto_increment  NOT NULL ,
         url          Varchar (255) NOT NULL ,
-        ref          Bool ,
+        ref          Boolean ,
         id_happening Int NOT NULL ,
+        PRIMARY KEY (id )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: orders
+#------------------------------------------------------------
+
+CREATE TABLE orders(
+        id            int (11) Auto_increment  NOT NULL ,
+        statute       Char (25) NOT NULL ,
+        purchase_date TimeStamp NOT NULL ,
+        id_account    Int NOT NULL ,
+        PRIMARY KEY (id )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: category
+#------------------------------------------------------------
+
+CREATE TABLE category(
+        id   int (11) Auto_increment  NOT NULL ,
+        name Varchar (255) NOT NULL ,
         PRIMARY KEY (id )
 )ENGINE=InnoDB;
 
@@ -117,6 +127,7 @@ CREATE TABLE love(
 #------------------------------------------------------------
 
 CREATE TABLE post_comment(
+        comment_date  TimeStamp NOT NULL ,
         id            Int NOT NULL ,
         id_commentary Int NOT NULL ,
         PRIMARY KEY (id ,id_commentary )
@@ -133,14 +144,28 @@ CREATE TABLE register(
         PRIMARY KEY (id ,id_happening )
 )ENGINE=InnoDB;
 
-ALTER TABLE selling ADD CONSTRAINT FK_selling_id_produce FOREIGN KEY (id_produce) REFERENCES produce(id);
-ALTER TABLE selling ADD CONSTRAINT FK_selling_id_account FOREIGN KEY (id_account) REFERENCES account(id);
+
+#------------------------------------------------------------
+# Table: order_composite
+#------------------------------------------------------------
+
+CREATE TABLE order_composite(
+        qantity    Int NOT NULL ,
+        id         Int NOT NULL ,
+        id_produce Int NOT NULL ,
+        PRIMARY KEY (id ,id_produce )
+)ENGINE=InnoDB;
+
 ALTER TABLE happening ADD CONSTRAINT FK_happening_id_account FOREIGN KEY (id_account) REFERENCES account(id);
 ALTER TABLE produce_picture ADD CONSTRAINT FK_produce_picture_id_produce FOREIGN KEY (id_produce) REFERENCES produce(id);
+ALTER TABLE produce ADD CONSTRAINT FK_produce_id_category FOREIGN KEY (id_category) REFERENCES category(id);
 ALTER TABLE event_picture ADD CONSTRAINT FK_event_picture_id_happening FOREIGN KEY (id_happening) REFERENCES happening(id);
+ALTER TABLE orders ADD CONSTRAINT FK_orders_id_account FOREIGN KEY (id_account) REFERENCES account(id);
 ALTER TABLE love ADD CONSTRAINT FK_love_id FOREIGN KEY (id) REFERENCES account(id);
 ALTER TABLE love ADD CONSTRAINT FK_love_id_happening FOREIGN KEY (id_happening) REFERENCES happening(id);
 ALTER TABLE post_comment ADD CONSTRAINT FK_post_comment_id FOREIGN KEY (id) REFERENCES account(id);
 ALTER TABLE post_comment ADD CONSTRAINT FK_post_comment_id_commentary FOREIGN KEY (id_commentary) REFERENCES commentary(id);
 ALTER TABLE register ADD CONSTRAINT FK_register_id FOREIGN KEY (id) REFERENCES account(id);
 ALTER TABLE register ADD CONSTRAINT FK_register_id_happening FOREIGN KEY (id_happening) REFERENCES happening(id);
+ALTER TABLE order_composite ADD CONSTRAINT FK_orders_composite_id FOREIGN KEY (id) REFERENCES orders(id);
+ALTER TABLE order_composite ADD CONSTRAINT FK_order_composite_id_produce FOREIGN KEY (id_produce) REFERENCES produce(id);
