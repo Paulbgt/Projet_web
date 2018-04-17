ff6aaa1fa9d47a8de614d4a486ad470c694c67ac
Aurélien Klein <aurelien.klein@viacesi.fr>
2018-04-17 16:12:05 +0200

    Création page boutique

---
+++ www/shop.php
@@ -0,0 +1,38 @@
+<?php session_start();  ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="icon" type="image/png" href="site_picture/exia-logo.png"/>
+    <title>BDE - Evénements terminés</title>
+    <meta name="description" content="Ecvénements du mois du BDE de l'Exia.Cesi d'Arras."/>
+    <link rel="stylesheet" href="AKLibrary/AURELIENKLEIN.library.min.css">
+    <link rel="stylesheet" href="css/common.min.css">
+    <link rel="stylesheet" href="css/eventDone.min.css">
+</head>
+<body>
+
+	<?php include '_header.php' ?>
+      
+      
+      <div class="wrapper">
+          <h1 class="AKL-ctn--c1 banner">Boutique</h1>
+          
+          
+          
+          
+          
+          
+          
+          
+      </div>
+       
+    <?php include '_footer.php' ?>
+    
+<script src="AKLibrary/AURELIENKLEIN.library.min.js"></script>
+<script defer src="js/common.min.js"></script>
+<script src="js/eventDone.min.js"></script>
+</body>
+</html>
