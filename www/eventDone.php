e7ce0df585ba14ac0359c961e4e80eceef0e3b04
florianfritsch <florian.fritsch@viacesi.fr>
2018-04-17 22:17:06 +0200

    ajout d'image sur des events existants

--- www/eventDone.php
+++ www/eventDone.php
@@ -93,20 +93,20 @@ $photos->execute(['id' => $response['id']]);
                     <p class="modamComment-comment-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores autem repellat nisi quaerat iusto aliquam dicta, libero soluta fugit ad atque excepturi, reiciendis cupiditate. Dignissimos, molestias! Repellendus, hic necessitatibus neque.</p>
                 </div>
             </div>$
+            <form action="php/add_photo_event_done.php" id="add_imageForm" method="POST" enctype="multipart/form-data">
             <div class="AKL-ctn--c2-s1 modalPhoto">
                 <label for="fileImgModalPhoto<?= $i ?>" class="AKL-btnClassic-Flat-ocean modalPhoto-inputLabel">Choisir une image</label>
-                <input type="file" id="fileImgModalPhoto<?= $i ?>" class="AKL-btnFile" class="modalPhoto-input" hidden>
+                <input type="file" name="image" id="fileImgModalPhoto<?= $i ?>" class="AKL-btnFile" class="modalPhoto-input" hidden>
                 <input type="submit" class="AKL-btnClassic-Flat-ocean modalPhoto-submit" value="Valider">
+            </form>
             </div>
         </div>
-
        
 <?php
 $i++; }
 $display->closeCursor();
 ?>
-        
-        
+               
     </div>
       
       
--- www/php/delete_event.php
+++ www/php/delete_event.php
@@ -17,7 +17,7 @@ $event_id = $_POST['event_id'];
 //on vérifie que le champs ne sont pas vide avant de remplir la base de données
 if (!empty($event_id)) {
     //requête permettant de supprimer l'événement
-    $delete = $bdd->prepare('DELETE FROM event WHERE id=:event_id LIMIT 1');
+    $delete = $bdd->prepare('DELETE FROM event WHERE id = :event_id LIMIT 1');
     $delete->execute([
         ':event_id' => $event_id
     ]);
@@ -25,6 +25,7 @@ if (!empty($event_id)) {
 //    Work to go back to previous page WITH reloading it, so changes appear
 //    header('Location: ' . $_SERVER['HTTP_REFERER']);
 //    Work to go back to previous page WHITOUT reloading it, so changes doesn't appear
+   
     header('Location: javascript://history.go(-1)'); 
 
     }else{
