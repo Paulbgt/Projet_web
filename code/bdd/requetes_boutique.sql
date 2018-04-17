bb9e32b48fb88c448d6557247423adaa7ceba6de
Paulbgt <paul.boogaerts@viacesi.fr>
2018-04-17 14:16:19 +0200

    Update requetes_boutique.sql

--- code/bdd/requetes_boutique.sql
+++ code/bdd/requetes_boutique.sql
@@ -16,20 +16,36 @@ insert into order_compsite (qantity, id_orders, id_produce) VALUES(':qantity',':
 :idproduce:input produce;
 
 
-/////afficher panier
-
-Select oders.id, from orders INNER JOIN order_composite ON orders.id=orders.order_composite where orders.id=:idorder;
-
-:idorder==$_SESSION['id_order'];
-
-
 
 ////// modifier quantite
 
-ALTER TABLE order_compsite (qantit) VALUES(':qantity') where id_order=:idorder AND id_produce=:idproduce;
+ALTER TABLE order_compsite (qantit) VALUES(':qantity') where id_orders=:idorder AND id_produce=:idproduce;
 
 :qantity=input qantity;
 :idorder=$_SESSION['id_order'];
 :idproduce:input produce;
 
-////suprimer contenu
+////suprimer un item du panier contenu
+DELETE FROM order_compsite where id_orders=:idorder AND id_produce=:idproduce; 
+
+:idorder==$_SESSION['id_order'];
+:idproduce:input produce;
+
+
+///suprimer un panier !!! attention il y a deux requetes 
+DELETE FROM order_compsite where id_orders=:idorder ; 
+DELETE FROM orders where id_orders=:idorder; 
+
+:idorder==$_SESSION['id_order'];
+
+
+
+////modifier le status 
+ALTER TABLE orders (statute) VALUES('attante') where id_orders=:idorder;
+:idorder==$_SESSION['id_order'];
+
+
+////voir le panier : 
+SELECT * from orders LEFT JOIN order_compsite ON orders.id=order_compsite.id_order WHERE orders.id=:idorder;
+:idorder==$_SESSION['id_order'];
+
