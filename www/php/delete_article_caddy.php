c5b842b93fec9142bdd1fa5a38200c3583444556
anthonydescamps <anthony.descamps@viacesi.fr>
2018-04-18 09:37:55 +0200

    Nous pouvons supprimer un article dans son panier
    Attention à bien changer le header

--- www/php/delete_article_caddy.php
+++ www/php/delete_article_caddy.php
@@ -1,3 +1,29 @@
 <?php session_start();  ?>
 <?php 
+
+try{
+//conexion à la base de données
+$bdd = new PDO('mysql:host=mysql-zeik.alwaysdata.net;dbname=zeik_web_project;charset=utf8', 'zeik_root', 'toor');
+
+} catch(PDOException $e){
+
+    die($e->getMessage());
+}
+
+$idorder = $_SESSION['id_order'];
+$idaccount = $_SESSION['id'];
+
+$a = $bdd->prepare('DELETE FROM order_composite where id_orders=:idorder LIMIT 1');
+$a->execute([
+':idorder' => $idorder
+]);
+
+header('Location: caddy.php');
+
 ?>
+
+
+
+
+
+
