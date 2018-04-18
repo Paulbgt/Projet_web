95dfb93962db396b02f66c52cdd2fe189effaed0
Paulbgt <paul.boogaerts@viacesi.fr>
2018-04-18 19:38:28 +0200

    ajout ajax v1

--- www/js/loadProduce.js
+++ www/js/loadProduce.js
@@ -1,6 +1,71 @@
+function clearArticle() {
+    document.querySelector('.listArticle').innerHTML = "";
+}
 
 
- var getJSON2 = function(url)
+function generateArticle(id, name, price, category, description, url) {
+    var listArticle = document.querySelector('.listArticle');
+    var art_form, article, art_img, art_name, art_infos, art_price, art_category, art_description, hidden, submit;
+
+    art_form = document.createElement('form');
+    art_form.setAttribute('action', "php/add_article_caddy.php");
+    art_form.setAttribute('method', "POST");
+
+    article = document.createElement('div');
+    article.className = "article";
+
+    art_img = document.createElement('div');
+    art_img.className = "AKL-ctn--c1 article-img";
+    art_img.style.backgroundImage = "url(../" + id + "/" + url + ")";
+
+    art_name = document.createElement('h3');
+    art_name.className = "article-img-title";
+    art_name.setAttribute('name', "shop-info-name");
+    art_name.innerHTML = name;
+
+    art_infos = document.createElement('div');
+    art_infos.className = "AKL-ctn--c1 article-infos";
+
+    art_price = document.createElement('span');
+    art_price.className = "article-infos-price";
+    art_price.setAttribute('name', "shop-info-price");
+    art_price.innerHTML = price;
+
+    art_category = document.createElement('span');
+    art_category.className = "article-infos-category";
+    art_category.innerHTML = category;
+
+    art_description = document.createElement('p');
+    art_description.className = "article-infos-description";
+    art_description.setAttribute('name', "shop-info-description");
+    art_description.innerHTML = description;
+
+    hidden = document.createElement('input');
+    hidden.setAttribute('name', "take_id_produce");
+    hidden.setAttribute('type', "hidden");
+    hidden.setAttribute('readonly', "true");
+    hidden.setAttribute('value', id);
+
+    submit = document.createElement('input');
+    submit.className = "AKL-btnModern-Shine-ocean article-infos-btnAdd";
+    submit.setAttribute('type', "submit");
+    submit.setAttribute('value', "+");
+
+    art_img.appendChild(art_name);
+    art_infos.appendChild(art_price);
+    art_infos.appendChild(art_category);
+    art_infos.appendChild(art_description);
+    art_infos.appendChild(hidden);
+    art_infos.appendChild(submit);
+
+    article.appendChild(art_img);
+    article.appendChild(art_infos);
+    art_form.appendChild(article);
+
+    listArticle.appendChild(art_form);
+}
+
+ function getJSON2(url)
  {
   return new Promise(function (data,err){
     var xhr = new XMLHttpRequest();
@@ -20,7 +85,7 @@
 };
 
 
-var getUrl = function(url,category,min,max)
+function getUrl(url,category,min,max)
 {                       url,null,
   if((category==null || category=="") && (min==null || min=="") && (max==null || max=="") )
   {
@@ -40,25 +105,25 @@ var getUrl = function(url,category,min,max)
 }
 
 
-const auteurId = function(data){
- for (var i = 0; i < data.length; i++) {
- console.log(data[i].id);
- }
- return data;
+var button=document.querySelector(".btnSearch");
+var minBtn=document.querySelector(".min-range");
+var maxBtn=document.querySelector(".max-range");
+var selectBtn=document.querySelector(".searchNav-select");
+
+button.addEventListener("click",function(){
+var min=minBtn.value;
+var max=maxBtn.value;
+var cat=selectBtn.value;
+console.log(min, max, cat );
+});
+
+function reload_produce()
+{
+  //createData(getJSON2(url))
 }
 
-const name = function(data){
- for (var i = 0; i < data.length; i++) {
-document.getElementById('auteur'+i).innerHTML = data[i].name;
- //console.log(data[i].name);
- }
- return data;
-}
-const avatar_urls = function(data){
- for (var i = 0; i < data.length; i++) {
- document.getElementById('avatar'+i).src = data[i].avatar_urls[96];
- }
- return data;
-}
-
-getJSON2(url+nbUser).then(auteurId).then(name).then(avatar_urls);
+function createData(data){
+  clearArticle();
+  for (var i = 0; i < data.length; i++) {
+  generateArticle(data[i].id,data[i].name,data[i].price,data[i].category,data[i].description, data[i].url);
+  }
