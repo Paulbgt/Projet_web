bf7dec48368711e92111a9ace36a1f8b3ca63ba0
anthonydescamps <anthony.descamps@viacesi.fr>
2018-04-19 16:56:22 +0200

    en attente

--- www/php/confirm_order.php
+++ www/php/confirm_order.php
@@ -3,7 +3,7 @@
 
 $db = new PDO('mysql:host=mysql-zeik.alwaysdata.net;dbname=zeik_web_project;charset=utf8', 'zeik_root', 'toor');
 
-$eza = $db->prepare('UPDATE orders SET statute = "attente" WHERE id_account = :idaccount)');
+$eza = $db->prepare('UPDATE orders SET statute = "attente" WHERE id_account = :idaccount');
 
 $eza->execute([
 	':idaccount' => $_SESSION['id']
