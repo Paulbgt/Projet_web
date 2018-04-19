7e5ed9e7e3051eb184e37c05b7d277c27bba7914
anthonydescamps <anthony.descamps@viacesi.fr>
2018-04-19 15:38:35 +0200

    download

--- www/zip.php
+++ www/zip.php
@@ -1,37 +1,35 @@
 <?php  
-//$id = $_GET['download_id'];
+
+
+
+$id = $_GET['download_id'];
+
+$pathdir = "event_picture/".$id."/";
+$nameArchive = time().".zip";
 
 		$zip = new ZipArchive; 
-		//on créer un nouveau fichier archive zip
-		//we create a new zip archive file
-		$zip->open("camarche.zip", ZipArchive::CREATE); 
-		//on lui donne un nom et on la créer avec la fonction CREATE
-		//we give it a name and create it with the CREATE function
 
-		$rep=scandir('event_picture'); 
-		//on liste les fichiers et dossiers dans le dossier selectionner
-		//we list the files and folders in the select folder
+if($zip ->open($nameArchive, ZipArchive::CREATE) === TRUE) {
+	
+	$dir = opendir($pathdir);
 
-		print_r($rep);
 
-		unset($rep[0], $rep[1], $rep[2000]); 
-		//permet de prendre jusqu'a 2000 photos dans le dossier
-		//take up to 200 photos in the folder
 
-		foreach ($rep as $file) {
-		chmod("event_picture", 0777);
+	while($file = readdir($dir)) {
+		if(is_file($pathdir.$file)) {
+			$zip ->addFile($pathdir.$file, $file);
 
-		$zip->addfile("event_picture");
-		//on ajoute le nom du fichier que l'on veut avoir en Zip
-		//we add the name of the file we want to have in Zip
 		}
+	}
+	header('Location: '.$nameArchive);
+	$zip ->close();
 
-		//header('Location:camarche.zip');
-		//on redirige vers le nom du zip pour que le client puisse le télécharger.
-		//we redirect to the name of the zip so that the client can download it.
+	echo "ça fonctionne";
+}
+else{
+	echo "ça ne fonctionne pas";
+}
 
-		$zip->close();
-		//on ferme l'archive.
-		//we close the archive
+
 
 ?>
